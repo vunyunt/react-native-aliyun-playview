a27b35b57ff9b40b3976763da6cce721eae596d2
vun <vunyunt@gmail.com>
2020-06-03 16:55:24 +0800

    Replace upload native module

--- android/src/main/java/com/example/aliyunplayview/RNAliyunPlayerPackage.java
+++ android/src/main/java/com/example/aliyunplayview/RNAliyunPlayerPackage.java
@@ -15,8 +15,9 @@ public class RNAliyunPlayerPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         return Arrays.<NativeModule>asList(new RNAliyunPlayerModule(reactContext),
-                new AliyunRecordModule(reactContext),
+                new AliyunVodUploadModule(reactContext),
                 new AliyunVodDownloadModule(reactContext));
+
     }
 
     // Deprecated from RN 0.47
